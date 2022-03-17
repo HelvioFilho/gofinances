9fdda3498ee17849f3a272d7d6821d1ee6fe00b4
Hélvio Filho <helviosvf@gmail.com>
2022-03-17 06:50:28 -0300

    Started navigation

--- App.tsx
+++ App.tsx
@@ -6,12 +6,11 @@ import {
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
 import AppLoading from 'expo-app-loading';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
 
@@ -25,9 +24,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
-      {/* <CategorySelect /> */}
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
